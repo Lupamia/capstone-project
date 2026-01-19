810c8db8875f4948e0a4bb801519ab8604f53c66
Lupamia <christianandrewlupamia.bsit@gmail.com>
2026-01-19 18:36:24 +0800

    Delete backend directory

--- backend/middleware/authMiddleware.js
+++
@@ -1,6 +0,0 @@
-exports.isAuthenticated = (req, res, next) => {
-  if (req.session && req.session.isAuthenticated && req.session.user) {
-    return next();
-  }
-  return res.status(401).json({ message: "Not authenticated" });
-};
